867c869353316ec4bc95d6cdd6b178bd94e0dbf6
Minhaj Shahid <shahid.minhaj@yahoo.com>
2019-06-03 20:17:29 -0400

    Created another function to display plugins

--- Base.py
+++ Base.py
@@ -27,6 +27,18 @@ class qbittorrent(object):
         # Return the client when calling the class
         return(client)
 
+    # Function to return the available plugins and/or update the current plugins
+    def plugins(self, update=False, verbose=False):
+        # Fetch and list the plugins used to search
+        plugins = client.search_plugins()
+        if verbose==True:
+            if len(plugins) >= 1:
+                print('List of available plugins:')
+                for i in range(0, len(plugins)):
+                    print('- '+plugins[i]['fullName'])
+            else:
+                raise Exception('No plugins detected')
+
     # Function to search for torrents and return a dictionary containing the results
     '''
         :param search_term: term or phrase to search for
@@ -35,7 +47,7 @@ class qbittorrent(object):
     def search(self, search_term=None, num_results=5):
         # Fetch and list the categories available to search for
         categories = client.search_categories(plugin_name='all')
-        print('List of available categories:')
+        print('\nList of available categories:')
         for i in categories:
             if i != '':
                 print('- '+i)
@@ -52,9 +64,16 @@ class qbittorrent(object):
         #         print('The status of the search is: %s' % status)
         # Stops running the search
         client.search_stop(search_id=search_id)
+        status = client.search_status(search_id=search_id)[0]
+        print('Status of search: %s' % status['status'])
         # Store result of search and return the list
         results = client.search_results(search_id=search_id, limit=5, offset=None)['results']
-        return(results)
+        # Raise an exception if results array is empty
+        if results == []:
+            raise Exception('Empty array for "results"')
+        else:
+            # Else print out the search results            
+            return(results)
 
 # Create an object 'qbit' of class qbittorrent
 qbit = qbittorrent()
@@ -63,13 +82,8 @@ client = qbit.init(verbose=True)
 
 # Testing the search function
 search_term = 'no country for old men'
-results = qbit.search(search_term=search_term, num_results=50)
-# Raise an exception if results array is empty
-if results == []:
-    raise Exception('Empty array for "results"')
-else:
-    # Else print out the search results
-    print('The results are: %s\n' % results)
+results = qbit.search(search_term=search_term, num_results=5)
+print('The results are: %s\n' % results)
 
 # Print out the list of current completed torrents
 print('Completed torrents:')
